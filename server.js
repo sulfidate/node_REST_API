dd8a5caa3e9617e66fa63898da027805e1e0e9f3
sulfidate <dev@sulfidate.rocks>
2024-03-23 12:33:45 +0100

    init, nodemon, starting scripts, http-server, first route to test

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const products = require('./data/products');
+
+
+const server = http.createServer((req, res) => {
+if(req.url === '/api/products' && req.method === 'GET'){
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(products))
+} else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route Not Found' }))
+}
+
+})
+
+const PORT = process.env.PORT || 5001
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))server.js
