4a220999a8d64ab69a75604d507f6734aad72780
sulfidate <dev@sulfidate.rocks>
2024-03-24 17:50:19 +0100

    mvc, GET all products, GET single product, install uuid

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const http = require('http');
-const products = require('./data/products');
-
+const { getProducts, getProduct, createProduct } = require('./controllers/productController')   
 
 const server = http.createServer((req, res) => {
 if(req.url === '/api/products' && req.method === 'GET'){
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(products))
+    getProducts(req, res)
+} else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+    const id = req.url.split('/')[3]
+    getProduct(req, res, id)
+} else if (req.url === '/api/products' && req.method === 'POST') {
+   createProduct(req, res)
 } else {
     res.writeHead(404, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({ message: 'Route Not Found' }))
@@ -15,4 +18,4 @@ if(req.url === '/api/products' && req.method === 'GET'){
 
 const PORT = process.env.PORT || 5001
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))server.js
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
