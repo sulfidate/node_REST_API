ada2355ce38b6ae876892ad279161396e59e7469
sulfidate <dev@sulfidate.rocks>
2024-03-25 15:21:50 +0100

    DELETE deleteProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -89,9 +89,30 @@ async function updateProduct(req, res, id) {
     }
 }
 
+// @desc    Delete Product
+// @route   DELETE /api/product/:id
+async function deleteProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ message: 'Product Not Found' }));
+        } else {
+            await Product.remove(id);
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({message: `Product ${id} removed`}));
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
     getProducts,
     getProduct,
     createProduct,
-    updateProduct
+    updateProduct,
+    deleteProduct
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const { getProducts, getProduct, createProduct, updateProduct } = require('./controllers/productController')   
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')   
 
 const server = http.createServer((req, res) => {
 if(req.url === '/api/products' && req.method === 'GET'){
@@ -12,6 +12,9 @@ if(req.url === '/api/products' && req.method === 'GET'){
 } else if (req.url.match(/\/api\/products\/([A-Za-z0-9]+)/) && req.method === 'PUT'){
     const id = req.url.split('/')[3]
     updateProduct(req, res, id)
+} else if (req.url.match(/\/api\/products\/([A-Za-z0-9]+)/) && req.method === 'DELETE'){
+    const id = req.url.split('/')[3]
+    deleteProduct(req, res, id)
 } else {
     res.writeHead(404, { 'Content-Type': 'application/json' })
     res.end(JSON.stringify({ message: 'Route Not Found' }))
